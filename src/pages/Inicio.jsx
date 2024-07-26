86342ced036513078ccae074e32d88f371f7a031
verozambrano <159626942+verozambrano@users.noreply.github.com>
2024-07-25 21:42:49 -0500

    Update Inicio.jsx

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function Inicio() {
-  return (
-    <div>
-      Inicio
+ return (
+    <div className="inicio-container">
+      <div className="inicio-text">
+        Bienvenido
+      </div>
     </div>
-  )
+  );
 }
 
 export default Inicio
