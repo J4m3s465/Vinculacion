42cde4912da04899dd21f21d42e7345646748346
verozambrano <159626942+verozambrano@users.noreply.github.com>
2024-07-27 19:43:17 -0500

    Update Inicio.jsx

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import '../styles/General.css';
 
 function Inicio() {
  return (
-    <div className="inicio-container">
-      <div className="inicio-text">
-        Bienvenido
-      </div>
+    <div className="container">
+      <h1 className="title">Bienvenido</h1>
+      <p className="paragraph">
+        Es una aplicación web desarrollada utilizando JavaScript y un framework moderno como React.
+       Proporciona una interfaz interactiva y dinámica, facilitando actualizaciones en tiempo real
+      </p>
     </div>
   );
 }
